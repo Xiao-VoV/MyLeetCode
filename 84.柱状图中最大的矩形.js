2c66fac85ae872eac045c7fb5c4d01223b8d93d6
X.P <a1679190@gmail.com>
2025-04-29 21:59:21 +0800

    添加第84题：柱状图中最大的矩形的解法

---
+++ 84.柱状图中最大的矩形.js
@@ -0,0 +1,72 @@
+/*
+ * @lc app=leetcode.cn id=84 lang=javascript
+ *
+ * [84] 柱状图中最大的矩形
+ *
+ * https://leetcode.cn/problems/largest-rectangle-in-histogram/description/
+ *
+ * algorithms
+ * Hard (47.30%)
+ * Likes:    2916
+ * Dislikes: 0
+ * Total Accepted:    520.3K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[2,1,5,6,2,3]'
+ *
+ * 给定 n 个非负整数，用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。
+ *
+ * 求在该柱状图中，能够勾勒出来的矩形的最大面积。
+ *
+ *
+ *
+ * 示例 1:
+ *
+ *
+ *
+ *
+ * 输入：heights = [2,1,5,6,2,3]
+ * 输出：10
+ * 解释：最大的矩形为图中红色区域，面积为 10
+ *
+ *
+ * 示例 2：
+ *
+ *
+ *
+ *
+ * 输入： heights = [2,4]
+ * 输出： 4
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 1
+ * 0
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var largestRectangleArea = function (heights) {
+    let maxArea = 0;
+    const stack = [];
+    heights = [0, ...heights, 0];
+    for (let i = 0; i < heights.length; i++) {
+        while (stack.length && heights[i] < heights[stack[stack.length - 1]]) {
+            let top = stack.pop();
+            maxArea = Math.max(
+                maxArea,
+                heights[top] * (i - stack[stack.length - 1] - 1)
+            );
+        }
+        stack.push(i);
+    }
+    return maxArea;
+};
+// @lc code=end
